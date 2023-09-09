ce10cb9e05b0505eec716c72cce72f40e80a7030
iceandollie <144558375+iceandollie@users.noreply.github.com>
2023-09-09 21:22:08 +0100

    ijdi]wajda

--- src/main/java/net/mcreator/revive/item/FireItem.java
+++ src/main/java/net/mcreator/revive/item/FireItem.java
@@ -4,7 +4,11 @@ package net.mcreator.revive.item;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.LivingEntity;
+
+import net.mcreator.revive.procedures.FirehitProcedure;
 
 public class FireItem extends SwordItem {
 	public FireItem() {
@@ -33,5 +37,12 @@ public class FireItem extends SwordItem {
 				return Ingredient.of();
 			}
 		}, 3, -3f, new Item.Properties());
+	}
+
+	@Override
+	public boolean hurtEnemy(ItemStack itemstack, LivingEntity entity, LivingEntity sourceentity) {
+		boolean retval = super.hurtEnemy(itemstack, entity, sourceentity);
+		FirehitProcedure.execute(entity);
+		return retval;
 	}
 }
