dd4ffdce24885939e2a742074bbc9d2c2e89b12d
iceandollie <icefighter86@gmail.com>
2023-09-10 12:28:22 +0200

    infermooagain

--- src/main/java/net/mcreator/revive/block/FragileMudBricksBlock.java
+++ src/main/java/net/mcreator/revive/block/FragileMudBricksBlock.java
@@ -6,7 +6,6 @@ import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
@@ -44,13 +43,13 @@ public class FragileMudBricksBlock extends Block {
 		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 		if (!dropsOriginal.isEmpty())
 			return dropsOriginal;
-		return Collections.singletonList(new ItemStack(Blocks.AIR));
+		return Collections.singletonList(new ItemStack(this, 1));
 	}
 
 	@Override
 	public void attack(BlockState blockstate, Level world, BlockPos pos, Player entity) {
 		super.attack(blockstate, world, pos, entity);
-		FragileMudBricksPlayerStartsToDestroyProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ());
+		FragileMudBricksPlayerStartsToDestroyProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ(), entity);
 	}
 
 	@Override
--- src/main/java/net/mcreator/revive/procedures/FragileMudBricksPlayerStartsToDestroyProcedure.java
+++ src/main/java/net/mcreator/revive/procedures/FragileMudBricksPlayerStartsToDestroyProcedure.java
@@ -1,10 +1,19 @@
 package net.mcreator.revive.procedures;
 
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.core.BlockPos;
 
 public class FragileMudBricksPlayerStartsToDestroyProcedure {
-	public static void execute(LevelAccessor world, double x, double y, double z) {
-		world.destroyBlock(BlockPos.containing(x, y, z), false);
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		if (!(EnchantmentHelper.getItemEnchantmentLevel(Enchantments.SILK_TOUCH, (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)) != 0)) {
+			world.destroyBlock(BlockPos.containing(x, y, z), false);
+		}
 	}
 }
